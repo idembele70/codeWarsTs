34ba31d2d4e8809a82a8f44781742dac8dd08434
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2022-06-02 04:43:06 +0200

    [fakeBin] using regex replace

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,5 @@
 "use strict";
 function fakeBin(x) {
-    return x
-        .split("")
-        .map(Number)
-        .map((x) => (x < 5 ? 0 : 1))
-        .join(``);
+    return x.replace(/[0-9]/g, (v) => (+v < 5 ? "0" : "1"));
 }
 console.log(fakeBin("45385593107843568"));
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 function fakeBin(x: string): string {
-  return x
-    .split("")
-    .map(Number)
-    .map((x) => (x < 5 ? 0 : 1))
-    .join(``);
+  return x.replace(/[0-9]/g, (v) => (+v < 5 ? "0" : "1"));
 }
 
 console.log(fakeBin("45385593107843568"));
