77f088baed7e01a2fcf79a754a0f467979cfb740
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2022-06-06 18:50:01 +0200

    [FirstNonConsecutive] using for iteration with if statement

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,12 @@
 "use strict";
 const BreakException = {};
 function FirstNonConsecutive(arr) {
-    let nonConsecutive = null;
-    arr.forEach((num, idx) => {
-        try {
-            if (num + 1 !== arr[idx + 1] && idx + 1 < arr.length) {
-                nonConsecutive = arr[idx + 1];
-                throw BreakException;
-            }
-        }
-        catch (error) {
-            if (error !== BreakException)
-                throw error;
-        }
-    });
-    return nonConsecutive;
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        const nextElement = arr[i + 1];
+        if (element + 1 !== nextElement)
+            return nextElement;
+    }
+    return null;
 }
 console.log(FirstNonConsecutive([9, 10, 12, 13, 14, 16, 17, 18]));
