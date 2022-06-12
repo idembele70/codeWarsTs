762ad7e1e4186b43f64b47e372fe1a1158ce900d
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2022-06-13 01:08:08 +0200

    [oddCount]
    Using if statement with modulo

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,7 @@
 "use strict";
-const BreakException = {};
-function FirstNonConsecutive(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        const element = arr[i];
-        const nextElement = arr[i + 1];
-        if (element + 1 !== nextElement)
-            return nextElement;
-    }
-    return null;
-}
-console.log(FirstNonConsecutive([9, 10, 12, 13, 14, 16, 17, 18]));
+console.log((function oddCount(n) {
+    if (n % 2 === 1)
+        return (n - 1) / 2;
+    else
+        return n / 2;
+})(74556));
