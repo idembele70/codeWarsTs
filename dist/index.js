0fbe4be128ea67d40d61042cd75dd5f76c6312ba
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2022-06-17 04:37:50 +0200

    [Solution Roman NumeRals Decoder]
    Using typed object and classic javascript method

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,19 @@
 "use strict";
-console.log((function oddCount(n) {
-    return (n % 2 === 0 ? n : n - 1) / 2;
-})(74556));
+function solution(roman) {
+    const numeralToNumber = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    };
+    const total = roman.split("").reduce((acc, cur, idx, array) => {
+        if (numeralToNumber[`${cur}`] < numeralToNumber[`${array[idx + 1]}`])
+            return acc - numeralToNumber[`${cur}`];
+        return acc + numeralToNumber[`${cur}`];
+    }, 0);
+    return total;
+}
+console.log(solution("MMVIII"));
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-console.log(
-  (function oddCount(n: number): number {
-    return (n % 2 === 0 ? n : n - 1) / 2;
-  })(74556)
-);
+function solution(roman: string): number {
+  const numeralToNumber: { [key: string]: number } = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  const total = roman.split("").reduce((acc, cur, idx, array) => {
+    if (numeralToNumber[`${cur}`] < numeralToNumber[`${array[idx + 1]}`])
+      return acc - numeralToNumber[`${cur}`];
+    return acc + numeralToNumber[`${cur}`];
+  }, 0);
+  return total;
+}
+
+console.log(solution("MMVIII"));
