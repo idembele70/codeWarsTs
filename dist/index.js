9897e6841e24d3c6cf5a6ce534210371bc3e614f
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2022-06-17 15:24:13 +0200

    [validatePin]
    using regex test and AND logical

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,4 @@
 "use strict";
-function solution(roman) {
-    const numeralToNumber = {
-        I: 1,
-        V: 5,
-        X: 10,
-        L: 50,
-        C: 100,
-        D: 500,
-        M: 1000,
-    };
-    const total = roman.split("").reduce((acc, cur, idx, array) => {
-        if (numeralToNumber[`${cur}`] < numeralToNumber[`${array[idx + 1]}`])
-            return acc - numeralToNumber[`${cur}`];
-        return acc + numeralToNumber[`${cur}`];
-    }, 0);
-    return total;
-}
-console.log(solution("MMVIII"));
+console.log((function validatePin(pin) {
+    return /\D/.test(pin) === false && (pin.length === 4 || pin.length === 6);
+})("-12345"));
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,5 @@
-function solution(roman: string): number {
-  const numeralToNumber: { [key: string]: number } = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000,
-  };
-
-  const total = roman.split("").reduce((acc, cur, idx, array) => {
-    if (numeralToNumber[`${cur}`] < numeralToNumber[`${array[idx + 1]}`])
-      return acc - numeralToNumber[`${cur}`];
-    return acc + numeralToNumber[`${cur}`];
-  }, 0);
-  return total;
-}
-
-console.log(solution("MMVIII"));
+console.log(
+  (function validatePin(pin: string): boolean {
+    return /\D/.test(pin) === false && (pin.length === 4 || pin.length === 6);
+  })("-12345")
+);
