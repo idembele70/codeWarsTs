6c89f025ff39fc04017b930f5f44c17abc283b97
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2022-06-17 15:28:15 +0200

    [validate pin]
    using regex replace method and AND Logical

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,6 @@
 "use strict";
 console.log((function validatePin(pin) {
-    return /\D/.test(pin) === false && (pin.length === 4 || pin.length === 6);
+    const pinLength = pin.length;
+    const isValidePin = pin.replace(/[^0-9]/, "").length === pin.length;
+    return [4, 6].indexOf(pinLength) !== -1 && isValidePin;
 })("-12345"));
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 console.log(
   (function validatePin(pin: string): boolean {
-    return /\D/.test(pin) === false && (pin.length === 4 || pin.length === 6);
+    const pinLength: number = pin.length;
+    const isValidePin: boolean =
+      pin.replace(/[^0-9]/, "").length === pin.length;
+    return [4, 6].indexOf(pinLength) !== -1 && isValidePin;
   })("-12345")
 );
