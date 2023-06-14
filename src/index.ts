4f351cb72d1018639c21bc54ebb0d24b6ebb5314
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2023-06-14 20:44:24 +0200

    Longest array func by comparing the first three words

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
-function squareSum(numbers: number[]): number {
-  return numbers.reduce(
-    (acc,cur)=> acc + cur ** 2,0
-  );
+function longestConsec(strarr: string[], k: number): string {
+  const strarrSorted = strarr.slice().sort((a, b) =>
+    b.length - a.length)
+  const longestArr = strarrSorted.slice(0, 2)
+  const isSameLength = longestArr.map(word => word.length).filter(w => w !== longestArr[0].length).length
+  if (isSameLength === 0)
+    return ''
+  else if (strarrSorted[1].length === strarrSorted[2].length)
+    return longestArr[0]
+  return longestArr.join('')
 }
-console.log(squareSum([1,2,2]))
+
+console.log(
+  longestConsec(
+    ['toto', 'titi', 'tata', 'mimi'], 2
+  )
+) //  abigailtheta
