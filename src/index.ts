89b8e311b1a387bc4cfbda58fe7a28b3bbe76cc7
idembele70 <71393282+idembele70@users.noreply.github.com>
2023-06-30 15:03:39 +0200

    3 files modified

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
 function longestConsec(strarr: string[], k: number): string {
-  const strarrSorted = strarr.slice().sort((a, b) =>
-    b.length - a.length)
-  const longestArr = strarrSorted.slice(0, 2)
-  const isSameLength = longestArr.map(word => word.length).filter(w => w !== longestArr[0].length).length
-  if (isSameLength === 0)
-    return ''
-  else if (strarrSorted[1].length === strarrSorted[2].length)
-    return longestArr[0]
-  return longestArr.join('')
+  const strarrSorted = strarr.slice().sort((a, b) => b.length - a.length);
+  const longestArr = strarrSorted.slice(0, k);
+  const isSameLength = longestArr
+    .map((word) => word.length)
+    .filter((w, i) => w !== longestArr[i + 1]?.length || 0);
+  console.log(isSameLength);
+  // if (isSameLength === 0)
+  //   return ''
+  // else if (strarrSorted[1].length === strarrSorted[2].length)
+  //   return longestArr[0]
+  return longestArr.join('');
 }
 
-console.log(
-  longestConsec(
-    ['toto', 'titi', 'tata', 'mimi'], 2
-  )
-) //  abigailtheta
+console.log(longestConsec(['toto', 'titi', 'tata', 'mimi'], 2)); //  abigailtheta
