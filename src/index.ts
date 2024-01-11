8309b1ca64a01ce5ff7055fbd38182ac8e607acc
idembele70 <idembele70@gmail.com>
2024-01-11 22:57:22 +0100

    feat: 5kyu Regex Password Validation

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,13 @@
-function longestConsec(strarr: string[], k: number): string {
-  const strarrWTDuplicate: string[] = []
-  strarr.forEach(el => {
-    const currentDuplicate =
-      strarrWTDuplicate.find(strarrEl => strarrEl.length === el.length)
-    if (currentDuplicate) {
-      const idx =
-        strarrWTDuplicate.findIndex(strarrEl => strarrEl.length === el.length)
-      if (idx >= 0)
-        strarrWTDuplicate[idx] = el
+export const REGEXP = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?!.*\W)\w{6,}/;
 
-    } else
-      strarrWTDuplicate.push(el)
-  })
-  const sortedWord = strarrWTDuplicate.slice().sort(
-    (a, b) => b.length - a.length
-  ).slice(0, k)
-  console.log(sortedWord)
-  const longestWord = []
-  for (const item of strarrWTDuplicate) {
-    if (sortedWord.includes(item))
-      longestWord.push(item)
-  }
-  return longestWord.join('')
-}
-const result0 = ["zone", "abigail", "theta", "form", "libe", "zas"]
-const nbr0 = 2
-const result1 = ["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"]
-const nbr1 = 1
-const result2 = [
-  'itvayloxrp',
-  'wkppqsztdkmvcuwvereiupccauycnjutlv',
-  'vweqilsfytihvrzlaodfixoyxvyuyvgpck'
-]
-const nbr2 = 2
-
-console.log(longestConsec(result2, nbr2)); //  abigailtheta
+console.log(REGEXP.test('fjd3IR9'), true);
+console.log(REGEXP.test('ghdfj32'), false);
+console.log(REGEXP.test('DSJKHD23'), false);
+console.log(REGEXP.test('dsF43'), false);
+console.log(REGEXP.test('4fdg5Fj3'), true);
+console.log(REGEXP.test('DHSJdhjsU'), false);
+console.log(REGEXP.test('fjd3IR9.;'), false);
+console.log(REGEXP.test('fjd3  IR9'), false);
+console.log(REGEXP.test('djI38D55'), true);
+console.log(REGEXP.test('djI3_8D55'), false);
+console.log(REGEXP.test('djI38D55@@'), false);
